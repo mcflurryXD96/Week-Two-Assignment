c2cece5a66e69389eb0a7a751856af1e8249d120
mcflurryxd96 <daniel.mcmurray@myemail.indwes.edu>
2015-09-16 19:01:34 +0000

    All_3_Program_Changes_Complete

--- F_&_C Table.py
+++ F_&_C Table.py
@@ -2,33 +2,13 @@
 __author__ = 'Daniel McMurray'
 
 # CIS 125
-# Fahrenheit & Celsius Table
+# Print Fahrenheit and Celsius Table
 #
 # This program computes and prints a table of Celsius temperatures and the Fahrenheit equivalents every 10 degrees from 0C to 100C.
 
-# Declare Magic Numbers and Constants
 
-# LOOPCOUNTER = 2
-# name = "bob"
-
-# Write program code here
-
-# ask user for a temp in Fahrenheit
-# Convert to Celsius
-# (F-32) * 5 / 9
-
-# Output answer to user
-
-
-# Input
-
-F = eval(input("Please enter a temp in Fahrenheit: "))
-
-C = (F-32) * 5 / 9
-
-print("The temp ", F, " in Fahrenheit is equal to ", C, " Celsius")
-
-
-# Process
-
-# Output
+def main():
+    for C in range(0,101,10):
+        F = (9/5 * C) + 32
+        print(C,F)
+main()
